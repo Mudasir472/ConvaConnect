0b7deba562d5c7192b437edd6f2effa2472a1e6b
MudasirBhat <bhatmuddu471@gmail.com>
2024-03-07 00:15:46 +0530

    navbar update

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,27 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import "./App.css";
+import { BrowserRouter as Router,  Route, Routes } from "react-router-dom";
+import { Home } from "./components/Pages/Home";
+import { About } from "./components/Pages/About";
+import { Blog } from "./components/Pages/Blog";
+import { Contact } from "./components/Pages/Contact";
+import Navbar from "./components/NavBar";
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <>
-      <h2>Mudasir Bhat</h2>
-      <h2>Fahad Masroor</h2>
-    </>
-  )
+      <Router>
+        <Navbar/>
+        <div className="pages">
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/about" element={<About />} />
+            <Route path="/blog" element={<Blog />} />
+            <Route path="/contact" element={<Contact />} />
+          </Routes>
+        </div>
+      </Router>
+  </>
+  );
 }
 
-export default App
+export default App;
