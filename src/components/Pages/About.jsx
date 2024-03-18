2c7d144d4ff88e3372542720420d80632fbeda4c
fahadi04 <fahadmasrur786@gmail.com>
2024-03-19 00:46:33 +0530

    Updating About Page

--- src/components/Pages/About.jsx
+++ src/components/Pages/About.jsx
@@ -1,10 +1,15 @@
 import React from "react";
 import AboutHead from "./AboutHead";
+import AboutSpec from "./AboutSpec";
 
 export const About = () => {
   return (
     <div className="about">
       <AboutHead aboutHead="Hi. We’re Your GoTo." aboutDesc="Since day one, we’ve helped people and businesses do their best work – simply and securely – from anywhere. Today, work and life are intertwined. At GoTo, we like to think of ourselves as your go-to. We help you focus on the things that matter most throughout the day: your projects, your professions, and even your personal passions."/> 
+
+      <AboutSpec />
     </div>
+
+     
   );
 };
