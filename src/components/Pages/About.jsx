7022e4a2008ad5a5a1d3c446babc4370da7d442c
MudasirBhat <bhatmuddu471@gmail.com>
2024-03-30 01:37:38 +0530

    updating About section

--- src/components/Pages/About.jsx
+++ src/components/Pages/About.jsx
@@ -1,13 +1,17 @@
 import React from "react";
 import AboutHead from "./AboutHead";
 import AboutSpec from "./AboutSpec";
+import AboutFuture from "./AboutFuture";
 
 export const About = () => {
   return (
     <div className="about">
-      <AboutHead aboutHead="Hi. We’re Your GoTo." aboutDesc="Since day one, we’ve helped people and businesses do their best work – simply and securely – from anywhere. Today, work and life are intertwined. At GoTo, we like to think of ourselves as your go-to. We help you focus on the things that matter most throughout the day: your projects, your professions, and even your personal passions."/> 
+      <AboutHead aboutHead="About Us." aboutDesc="Since day one, we’ve helped people and businesses do their best work – simply and securely – from anywhere. Today, work and life are intertwined. At GoTo, we like to think of ourselves as your go-to. We help you focus on the things that matter most throughout the day: your projects, your professions, and even your personal passions."/> 
 
       <AboutSpec />
+
+      <AboutFuture />
+
     </div>
 
      
---
+++ src/components/Pages/AboutFuture.jsx
@@ -0,0 +1,27 @@
+
+import './AboutFuture.css'
+import img from './Images/AboutFuture.png'
+export default function(){
+    return(
+        <>
+            <div className="aboutFuture d-flex justify-content-center align-items-center mt-5 mb-5">
+                <div className="left">
+                    <img src={img} alt="" />
+                </div>
+                <div className="right d-flex justify-content-start flex-column align-items-start">
+                    <div className="heading">
+                        <h3>The future of work is now</h3>
+                    </div>
+                    <div >
+                        <p style={{width: "80%"}}>We're witnessing an evolution of the modern workforce, and the revolution of the modern workplace. GoTo is at the forefront, ready to help everyone take on all challenges, including:</p>
+                        <p className='d-flex flex-column align-items-start'>
+                            <li>Facilitating flexible, hybrid and remote work policies</li>
+                            <li>Providing seamless, on-demand support and assistance</li>
+                            <li>Delivering powerful collaboration tools and cybersecurity products</li>
+                        </p>
+                    </div>
+                </div>
+            </div>
+        </>
+    )
+}
