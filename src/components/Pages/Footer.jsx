0b193573939807f623088133bf1aecf2996011cd
fahadi04 <fahadmasrur786@gmail.com>
2024-04-02 00:24:08 +0530

    update Footer Section

---
+++ src/components/Pages/Footer.jsx
@@ -0,0 +1,68 @@
+import "./Footer.css"
+
+export default function () {
+    return (
+        <>
+            <div style={{color : "white"}} className="footer container">
+                <footer class="py-5 mt-4">
+                    <div class="row justify-content-between">
+                        <div class="col-6 col-md-2 mb-3">
+                            <h5>About ConvoConnect</h5>
+                            <ul class="nav flex-column">
+                                <li class="nav-item mb-2"><a  href="#" class="nav-link p-0">Mission and Vision</a></li>
+                                <li class="nav-item mb-2"><a href="#" class="nav-link p-0">Blog</a></li>
+                                <li class="nav-item mb-2"><a href="#" class="nav-link p-0">Contact Us</a></li>
+                                <li class="nav-item mb-2"><a href="#" class="nav-link p-0">Feedback</a></li>
+                                <li class="nav-item mb-2"><a href="#" class="nav-link p-0">FAQ</a></li>
+                            </ul>
+                        </div>
+
+                        <div class="col-6 col-md-2 mb-3">
+                            <h5>Features</h5>
+                            <ul class="nav flex-column">
+                                <li class="nav-item mb-2"><a href="#" class="nav-link p-0">Attendance Tracking</a></li>
+                                <li class="nav-item mb-2"><a href="#" class="nav-link p-0">Attendance Tracking</a></li>
+                                <li class="nav-item mb-2"><a href="#" class="nav-link p-0">Mobile Accessibility</a></li>
+                                <li class="nav-item mb-2"><a href="#" class="nav-link p-0">End-to-End Encryption</a></li>
+                                <li class="nav-item mb-2"><a href="#" class="nav-link p-0">Session Analytics</a></li>
+                            </ul>
+                        </div>
+
+                        <div class="col-6 col-md-2 mb-3">
+                            <h5>Resources</h5>
+                            <ul class="nav flex-column">
+                                <li class="nav-item mb-2"><a href="#" class="nav-link p-0 ">Video Tutorials</a></li>
+                                <li class="nav-item mb-2"><a href="#" class="nav-link p-0 ">Product Demos</a></li>
+                                <li class="nav-item mb-2"><a href="#" class="nav-link p-0 ">Software Updates</a></li>
+                                <li class="nav-item mb-2"><a href="#" class="nav-link p-0 ">Remote Work Tips</a></li>
+                                <li class="nav-item mb-2"><a href="#" class="nav-link p-0 ">Customer Support Porta</a></li>
+                            </ul>
+                        </div>
+                        <div class="col-6 col-md-2 mb-3">
+                            <h5>Connect with Us</h5>
+                            <ul class="nav flex-column">
+                                <li class="nav-item mb-2"><a href="#" class="nav-link p-0 ">Follow Us on Social Media</a></li>
+                                <li class="nav-item mb-2"><a href="#" class="nav-link p-0 ">Contact Information</a></li>
+                                <li class="nav-item mb-2"><a href="#" class="nav-link p-0 ">Report a Bug</a></li>
+                                <li class="nav-item mb-2"><a href="#" class="nav-link p-0 ">Give Us Your Suggestions</a></li>
+                                <li class="nav-item mb-2"><a href="#" class="nav-link p-0 ">Refer a Friend</a></li>
+                            </ul>
+                        </div>
+                    </div>
+
+                    <div class="d-flex flex-column flex-sm-row justify-content-between py-4 my-4 border-top">
+                        <p>© 2024 Company, Inc. All rights reserved.</p>
+                        <ul class=".text-light list-unstyled d-flex">
+                            <li class="ms-3"><a class="link-body-emphasis" href="#"><i style={{color : 'white'}} class="bi bi-facebook"></i></a></li>
+                            <li class="ms-3"><a class="link-body-emphasis" href="#"><i style={{color : 'white'}} class="bi bi-facebook"></i></a></li>
+                            <li class="ms-3"><a class="link-body-emphasis" href="#"><i style={{color : 'white'}} class="bi bi-facebook"></i></a></li>
+                            <li class="ms-3"><a class="link-body-emphasis" href="#"><i style={{color : 'white'}} class="bi bi-facebook"></i></a></li>
+                            
+                            
+                        </ul>
+                    </div>
+                </footer>
+            </div>
+        </>
+    )
+}
